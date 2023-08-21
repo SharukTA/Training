eb17849cb9db5d2dce3f6635a512a5eaa6aa85d7
SharukTA <138190936+SharukTA@users.noreply.github.com>
2023-08-21 19:54:03 +0530

    Add files via upload

--- Training/script.js
+++ Training/script.js
@@ -1,59 +1,65 @@
 let email = document.getElementById("myInput5");
-  let phone = document.getElementById("myInput4");
-  let password = document.getElementById("myInput8");
-  let confirmPassword = document.getElementById("myInput9");
-  let form = document.getElementById("registrationForm");
+let phone = document.getElementById("myInput4");
+let password = document.getElementById("myInput8");
+let confirmPassword = document.getElementById("myInput9");
+let form = document.getElementById("registrationForm")
 
-
-  form.addEventListener("submit", function (event) {
-        if (!isValidEmail(email.value)) {
-          event.preventDefault();
-          alert("Please enter a valid email address.");
-        } else if (!isValidnumber(phone.value)) {
-          event.preventDefault();
-          alert("Please enter a valid phone number.");
-        } else if (password.value !== confirmPassword.value) {
-          event.preventDefault();
-          alert("Passwords do not match. Please re-enter your password.");
-        } else {
-          alert("Registration Successfull");
-        }
-    });
-
-    function isValidEmail(email) {
-        let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-        return emailPattern.test(email);
+function validateForm() {
+    
+    if (!isValidEmail(document.form1.email.value)) {
+        alert("Please enter a valid email address.");
+        return false;
     }
-    function isValidnumber(number){
-        const phonePattern = /^[0-9]{3}[-\s]?[0-9]{3}[-\s]?[0-9]{4}$/;
-        return phonePattern.test(number);
-
+    
+    if (!isValidPhoneNumber(document.form1.Phone.value)) {
+        alert("Please enter a valid phone number.");
+        return false;
     }
 
+    if (!isPasswordMatch()) {
+        alert("Passwords do not match.");
+        return false;
+    }
 
+    alert("Registration Successful!");
+}
+function isValidEmail(email) {
+    let emailformat = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailformat.test(email);
+}
 
-  let citySelect = document.getElementById("city");
-  citySelect.addEventListener("focusin", myFocusFunction);
-  citySelect.addEventListener("focusout", myBlurFunction);
+function isValidPhoneNumber(phone) {
+    var numformat = /^[0-9]{3}[-\s]?[0-9]{3}[-\s]?[0-9]{4}$/;
+    return numformat.test(phone);
+}
 
-  let stateSelect = document.getElementById("state");
-  stateSelect.addEventListener("focusin", myFocusFunction);
-  stateSelect.addEventListener("focusout", myBlurFunction);
+function isPasswordMatch() {
+    var password = document.form1.psw.value;
+    var confirmPassword = document.form1.confirmPsw.value;
+    return password === confirmPassword;
+}
+let citySelect = document.getElementById("city");
+citySelect.addEventListener("focusin", myFocusFunction);
+citySelect.addEventListener("focusout", myBlurFunction);
 
-  let dateofbirth = document.getElementById("dob");
-  dateofbirth.addEventListener("focusin", myFocusFunction);
-  dateofbirth.addEventListener("focusout", myBlurFunction);
+let stateSelect = document.getElementById("state");
+stateSelect.addEventListener("focusin", myFocusFunction);
+stateSelect.addEventListener("focusout", myBlurFunction);
 
-  for (let i = 1; i <= 11; i++) {
-      let input = document.getElementById("myInput" + i);
-      input.addEventListener("focusin", myFocusFunction);
-      input.addEventListener("focusout", myBlurFunction);
-  }
+let dateofbirth = document.getElementById("dob");
+dateofbirth.addEventListener("focusin", myFocusFunction);
+dateofbirth.addEventListener("focusout", myBlurFunction);
 
-  function myFocusFunction() {
-      this.style.backgroundColor = "bisque";
-  }
+for (let i = 1; i <= 11; i++) {
+    let input = document.getElementById("myInput" + i);
+    input.addEventListener("focusin", myFocusFunction);
+    input.addEventListener("focusout", myBlurFunction);
+}
 
-  function myBlurFunction() {
-      this.style.backgroundColor = "";
-  }
+function myFocusFunction() {
+    this.style.backgroundColor = "bisque";
+}
+
+function myBlurFunction() {
+    this.style.backgroundColor = "";
+}
