291e3b4254c5a9de64313955d0e7b134008c993e
SharukTA <138190936+SharukTA@users.noreply.github.com>
2023-08-25 21:04:53 +0530

    Add files via upload

--- Training/script.js
+++ Training/script.js
@@ -1,43 +1,129 @@
 let email = document.getElementById("myInput5");
+const emailerror = document.getElementById("email-error");
+const emailpattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 let phone = document.getElementById("myInput4");
+const phoneerror = document.getElementById("phone-error");
 let password = document.getElementById("myInput8");
+const passworderror = document.getElementById("password-error");
 let confirmPassword = document.getElementById("myInput9");
-let form = document.getElementById("registrationForm")
+const cpswerror = document.getElementById("cpassword-error");
+let form = document.getElementById("form1")
 
-function validateForm() {
-    
-    if (!isValidEmail(document.form1.email.value)) {
-        alert("Please enter a valid email address.");
-        return false;
+email.addEventListener("focusout",function()
+{
+    if(!emailpattern.test(email.value)){
+        emailerror.style.display="block";
+        email.style.borderColor="red";
     }
-    
-    if (!isValidPhoneNumber(document.form1.Phone.value)) {
-        alert("Please enter a valid phone number.");
-        return false;
+    else{
+        emailerror.style.display="none";
+        email.style.borderColor="";
+    }
+});
+
+phone.addEventListener("focusout",function()
+{
+    if(phone.value.length!=10){
+        phoneerror.style.display="block";
+        phone.style.borderColor="red";
+    }
+    else{
+        phoneerror.style.display="none";
+        phone.style.borderColor="";
+    }
+});
+
+function calculateAge(dateOfBirth) {
+    const today = new Date();
+    const dob = new Date(dateOfBirth);
+    let age = today.getFullYear() - dob.getFullYear();
+    const monthDiff = today.getMonth() - dob.getMonth();
+
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dob.getDate())) {
+        age--;
     }
 
-    if (!isPasswordMatch()) {
-        alert("Passwords do not match.");
-        return false;
+    return age;
+}
+
+
+const dobInput = document.getElementById("dob");
+const ageInput = document.getElementById("myInput3");
+
+dobInput.addEventListener("change", function() {
+    const age = calculateAge(this.value);
+    ageInput.value = age;
+});
+
+
+const initialAge = calculateAge(dobInput.value);
+ageInput.value = initialAge;
+
+
+password.addEventListener("focusout",function()
+{
+    if(password.value.length<8){
+        passworderror.style.display="block";
+        password.style.borderColor="red";
     }
+    else{
+        passworderror.style.display="none";
+        password.style.borderColor="";
+    }
+});
 
-    alert("Registration Successful!");
-}
-function isValidEmail(email) {
-    let emailformat = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    return emailformat.test(email);
+confirmPassword.addEventListener("focusout",function()
+{
+    if(confirmPassword.value === password.value){
+        cpswerror.style.display="none";
+        confirmPassword.style.borderColor="";
+    }
+    else{
+        cpswerror.style.display="block";
+        confirmPassword.style.borderColor="red";
+    }
+});
+
+const citiesByState = {
+    Kerala: ["Kochi", "Thrissur","Trivandrum", "Malappuram"],
+    TamilNadu: ["Chennai", "Salem","Erode", "Coimbatore"],
+    Karnataka: ["Bangalore", "Belur", "Whitefield", "Electronic city","100 feet"]
+};
+
+function populateCities() {
+    const stateDropdown = document.getElementById("state");
+    const cityDropdown = document.getElementById("city");
+
+    const selectedState = stateDropdown.value;
+    const cities = citiesByState[selectedState];
+
+    cityDropdown.innerHTML = "";
+    for (const city of cities) {
+        const option = document.createElement("option");
+        option.text = city;
+        cityDropdown.add(option);
+    }
 }
 
-function isValidPhoneNumber(phone) {
-    var numformat = /^[0-9]{3}[-\s]?[0-9]{3}[-\s]?[0-9]{4}$/;
-    return numformat.test(phone);
-}
+populateCities();
 
-function isPasswordMatch() {
-    var password = document.form1.psw.value;
-    var confirmPassword = document.form1.confirmPsw.value;
-    return password === confirmPassword;
-}
+form.addEventListener("submit", function(event) {
+    if (
+        email.style.borderColor === "red" ||
+        phone.style.borderColor === "red" ||
+        confirmPassword.style.borderColor === "red" ||
+        password.style.borderColor === "red"
+    ) {
+        alert("Please fix the validation errors before submitting the form.");
+        event.preventDefault();
+    }
+    else{
+        alert("registration successfull");
+        
+
+    }
+});
+
 let citySelect = document.getElementById("city");
 citySelect.addEventListener("focusin", myFocusFunction);
 citySelect.addEventListener("focusout", myBlurFunction);
@@ -63,3 +149,4 @@ function myFocusFunction() {
 function myBlurFunction() {
     this.style.backgroundColor = "";
 }
+
